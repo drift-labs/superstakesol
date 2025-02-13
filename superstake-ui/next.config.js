de7cae021803f4aafdded74c9ae157ff2616b746
Luke Steyn <lukecaan@gmail.com>
2025-02-13 17:23:56 +0800

    sigh

--- superstake-ui/next.config.js
+++ superstake-ui/next.config.js
@@ -3,9 +3,15 @@ const nextConfig = {
 	typescript: { ignoreBuildErrors: true },
 
 	output: 'standalone',
+
+	// <Force dynamic rendering>
+	dynamicParams: true,
 	experimental: {
 		disableStaticGeneration: true,
-	}, // The above two params are an attempt to fix a build error during "static site generation" in Vercel which I can't solve and doesn't seem worth spending more time fixing considering we're going to decommission superstake soon anyway.
+	},
+	generateStaticParams: false,
+	unstable_runtimeJS: true,
+	// </Force dnamic rendering> :: The above params are an attempt to fix a build error during "static site generation" in Vercel which I can't solve and doesn't seem worth spending more time fixing considering we're going to decommission superstake soon anyway.
 
 	webpack(config, { isServer }) {
 		if (!isServer) {
